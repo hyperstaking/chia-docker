5471e626be8fb5a5d4a6856b1c984eebadb4d6c9
marcelhodan <marcel.hodan@gmail.com>
2021-07-27 11:54:47 +0200

    move recursive plot adding to end of script
    this way the debug.log will be available much eariler.

--- entrypoint.sh
+++ entrypoint.sh
@@ -31,9 +31,6 @@ for p in ${plots_dir//:/ }; do
     chia plots add -d ${p}
 done
 
-# original cmd, without recursive search for plots. chia plots add -d ${plots_dir}
-find ${plots_dir} -iname "*.plot" -type f | xargs -n 1 -I[] dirname [] | grep -v "Trash" | sort --unique | xargs -n 1 -I[] chia plots add -d []
-
 sed -i 's/localhost/127.0.0.1/g' ~/.chia/mainnet/config/config.yaml
 # use DEBUG as default log_level
 sed -i 's/log_level: WARNING/log_level: DEBUG/g' /root/.chia/mainnet/config/config.yaml
@@ -59,5 +56,8 @@ if [[ ${testnet} == "true" ]]; then
     chia configure --set-fullnode-port ${var.full_node_port}
   fi
 fi
+
+# original cmd, without recursive search for plots. chia plots add -d ${plots_dir}
+find ${plots_dir} -iname "*.plot" -type f | xargs -n 1 -I[] dirname [] | grep -v "Trash" | sort --unique | xargs -n 1 -I[] chia plots add -d []
 
 while true; do sleep 30; done;
